39a00f32c008bf05cbdf45a81c286a70b3f1a840
kabboss <147599900+kabboss@users.noreply.github.com>
2025-03-12 12:51:28 +0000

    Add files via upload

--- netlify/functions/get-update.js
+++ netlify/functions/get-update.js
@@ -21,7 +21,7 @@ exports.handler = async (event) => {
                 "Access-Control-Allow-Headers": "Content-Type"
             },
             body: JSON.stringify({
-                downloadUrl: "https://drive.google.com/uc?export=download&id=12zATa4beMkU8AcavDwTYbWJP4JLg2kpH",
+                downloadUrl: "https://drive.google.com/file/d/1hB4kppnFgFTfZ22bLt56ou1Pu2RlkTOj/view?usp=sharing",
                 message: `📱 Mise à jour 🔄\n Publiée le : 12 mars 2025📅 .`,
             }),
         };
